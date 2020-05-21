e2b756e31c56873fd120a7eee0657ed39cf896d0
messerc <messer55@gmail.com>
2020-05-21 14:33:25 -0700

    quick adjustments - read from URL (of course) - start some basic hooking up now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  casesURL,
+  deathsURL,
+  recoveredURL,
+} from './constants';
+import Papa from 'papaparse';
 import FilterBar from './FilterBar';
 import LineChart from './LineChart';
 import ReportedCasesByCountry from './ReportedCasesByCountry';
 
 function App() {
+
+  useEffect(() => {
+    Papa.parse(casesURL, {
+      download: true,
+      header: true,
+      complete: results => console.log(results),
+    });
+  }, [])
+
   return (
     <div>
       <h1>Covid 19 site</h1>
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const casesURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+export const deathsURL = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
+export const recoveredURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
+
+export default {
+    casesURL,
+    deathsURL,
+    recoveredURL,
+};
